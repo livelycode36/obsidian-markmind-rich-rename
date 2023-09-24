ff5c64d8a329a30b19305a874d963a5d3e35492c
shengwei <414685209@qq.com>
2023-09-24 13:51:15 +0800

    test(markmind): 简单正则实现

--- main.ts
+++ main.ts
@@ -1,78 +1,142 @@
-import {Plugin, TAbstractFile} from 'obsidian';
+import {Notice, Plugin, TAbstractFile, TFile, TFolder} from 'obsidian';
 import {debugLog} from "./src/log";
+import {path} from "./src/path";
+import {getMetadata} from 'src/metadata';
 
 export default class MarkmindRenamePlugin extends Plugin {
+    async onload() {
+        this.registerEvent(
+            // 在重命名文件夹时触发重命名事件，重命名文件夹中的每个文件/文件夹（包括其本身）都将触发该事件
+            this.app.vault.on("rename", async (file: TAbstractFile, oldPath: string) => {
+                this.renameEventProcessor(file, oldPath);
+            })
+        );
+    }
 
-	async onload() {
-		await this.loadSettings();
+    private renameEventProcessor(newFile: TAbstractFile | TFile, oldPath: string) {
+        // debugLog("on rename event - new path and old path:", file.path, oldPath);
+        // debugLog("new file:", file);
+        // debugLog("new file:", getMetadata(file.path));
 
-		this.registerEvent(
-			// 在重命名文件夹时触发重命名事件，重命名文件夹中的每个文件/文件夹（包括其本身）都将触发该事件
-			this.app.vault.on("rename", async (file: TAbstractFile, oldPath: string) => {
-				debugLog("on rename event - new path and old path:", file.path, oldPath);
+        if (newFile instanceof TFile) {
+            if (
+                path.basename(oldPath, path.extname(oldPath)) ===
+                path.basename(newFile.path, path.extname(newFile.path))
+            ) {
+                debugLog("拖拽文件")
 
-				// 将对象格式化为json字符串
-				debugLog("on rename event - file:",file);
+                let markdownFiles = this.app.vault.getMarkdownFiles();
+                markdownFiles.forEach((mdFile) => {
+                    this.app.fileManager.processFrontMatter(mdFile, frontmatter => {
+                        // 找到markmind文件
+                        if (this.isMarkmindRich(frontmatter)) {
+                            this.app.vault.read(mdFile)
+                                .then((content) => {
+                                    debugLog(`当前文件：${mdFile.name}`)
+                                    // debugLog("内容：")
+                                    // debugLog(content)
+                                    // debugLog("内容结束")
 
+                                    let match;
 
-				// if (!this.settings.autoRenameAttachment) {
-				//     debugLog("rename - auto rename not enabled:", this.settings.autoRenameAttachment);
-				//     return;
-				// }
-				//
-				// const type = ATTACHMENT_RENAME_TYPE.BOTH;
-				// debugLog("rename - attachRenameType:", type);
-				// // if (type === ATTACHMENT_RENAME_TYPE.NULL) {
-				// //     debugLog("rename - no variable use, skipped");
-				// //     return;
-				// // }
-				//
-				// if (file instanceof TFile) {
-				//     if (file.parent && isExcluded(file.parent.path, this.settings)) {
-				//         debugLog("rename - exclude path:", file.parent.path);
-				//         new Notice(`${file.path} was excluded, skipped`);
-				//         return;
-				//     }
-				//     // 如果重命名的文件是附件，则跳过
-				//     if (isAttachment(this.settings, file)) {
-				//         debugLog("rename - not processing rename on attachment:", file.path);
-				//         return;
-				//     }
-				//
-				//     let eventType: RenameEventType;
-				//     if (
-				//         path.basename(oldPath, path.extname(oldPath)) ===
-				//         path.basename(file.path, path.extname(file.path))
-				//     ) {
-				//         // 重命名文件夹事件
-				//         eventType = RENAME_EVENT_TYPE_FOLDER;
-				//         debugLog("rename - RENAME_EVENT_TYPE:", RENAME_EVENT_TYPE_FOLDER);
-				//     } else {
-				//         // 重命名文件事件
-				//         eventType = RENAME_EVENT_TYPE_FILE;
-				//         debugLog("rename - RENAME_EVENT_TYPE:", RENAME_EVENT_TYPE_FILE);
-				//     }
-				//
-				//     // debugLog("rename - overrideSetting:", setting);
-				//     const processor = new RenameHandler(this.app, this.settings, setting);
-				//     await processor.onRename(file, oldPath, eventType, type);
-				// } else if (file instanceof TFolder) {
-				//     // 忽略文件夹的重命名事件
-				//     // debugLog("rename - ignore rename folder event:", file.name, oldPath);
-				//     return;
-				// }
-			}),
-		);
-	}
+                                    // wiki链接：[[文件名（不带后缀）]]、[[路径/文件名（不带后缀）]]
+                                    const wikiLinkRegex = /!?\[\[(.*?)]]/g;
+                                    // const wikiLinkRegex = /!?\[\[((?!\[\[).*)]]/g;
+                                    while ((match = wikiLinkRegex.exec(content)) !== null) {
+                                        // debugLog("当前匹配的全内容:", match[0]);
+                                        // debugLog("链接中的地址数据:", match[1]);
 
-	onunload() {
+                                        let linkOldPath = match[1]
+                                        let oldPathMetadata = getMetadata(oldPath);
 
-	}
+                                        // 判断是否，当前的重命名文件的旧链接
+                                        // debugger
+                                        if (linkOldPath.includes(oldPathMetadata.basename)) {
+                                            // 判断是 路径/文件名 还是 文件名
+                                            if (linkOldPath.includes('/')) {
+                                                debugLog("重命名wiki链接 - 文件夹/文件:");
+                                                debugLog("旧链接：", linkOldPath);
+                                                debugLog("新链接：", this.removeExtension(newFile.path));
+                                                content = content.replace(linkOldPath, this.removeExtension(newFile.path))
+                                            } else {
+                                                debugLog("重命名wiki链接 - 文件:");
+                                                debugLog("旧链接：", linkOldPath);
+                                                debugLog("新链接：", newFile.basename);
+                                                content = content.replace(linkOldPath, newFile.basename)
+                                            }
+                                        }
+                                    }
 
-	async loadSettings() {
-	}
+                                    // markdown链接：[名称](地址)
+                                    const mdLinkRegex = /!?\[.*?]\((.*?)\)/g;
+                                    while ((match = wikiLinkRegex.exec(content)) !== null) {
+                                        // debugLog("当前匹配的全内容:", match[0]);
+                                        // debugLog("链接中的地址数据:", match[1]);
 
-	async saveSettings() {
-		// await this.saveData(this.settings);
-	}
+                                        let linkOldPath = match[1]
+                                        if (linkOldPath.includes(oldPath)) {
+                                            let oldLink = match[0]
+                                            let newLink = match[0].replace(match[1], newFile.path)
+                                            content = content.replace(linkOldPath, newFile.path)
+
+                                            debugLog("重命名markdown链接:");
+                                            debugLog("旧链接：", oldLink);
+                                            debugLog("新链接：", newLink);
+                                            debugLog("更新后的内容：",content);
+                                        }
+                                    }
+
+                                    // markmind的节点链接
+                                    const markmindNodeLinkRegex = /obsidian:\/\/jump-to-pdf\?md=(.*)\.md&node=.*\)/g;
+                                    while ((match = markmindNodeLinkRegex.exec(content)) !== null) {
+
+                                        let linkOldPath = match[1]
+                                        // 将 路径 + 文件名（去掉扩展名），进行url编码
+                                        if (linkOldPath.includes(encodeURIComponent(this.removeExtension(oldPath)))) {
+                                            let oldLink = match[0]
+                                            let newLink = match[0].replace(match[1], encodeURIComponent(this.removeExtension(newFile.path)))
+
+                                            content = content.replace(oldLink, newLink)
+                                            // debugLog("重命名markdown链接:");
+                                            // debugLog("旧链接：",oldLink);
+                                            // debugLog("新链接：",newLink);
+                                            // debugLog("更新后的内容：",content);
+
+                                            this.app.vault.modify(newFile, content)
+                                            new Notice(`该文件 ${newFile.basename} 中的 markmind的节点链接 已更新！`);
+                                        }
+                                    }
+                                })
+                        }
+                    })
+                })
+            } else {
+                // 重命名文件事件
+                debugLog("重命名文件")
+            }
+        } else if (newFile instanceof TFolder) {
+            // 重命名文件夹事件
+            debugLog("重命名文件夹")
+            return;
+        }
+    }
+
+    private isMarkmindRich(frontmatter: any) {
+        return JSON.stringify(frontmatter) != '{}' && frontmatter['mindmap-plugin'] == 'rich';
+    }
+
+    private removeExtension(filename: string) {
+        return filename.substring(0, filename.lastIndexOf('.')) || filename;
+    }
+
+    onunload() {
+
+    }
+
+    async loadSettings() {
+    }
+
+    async saveSettings() {
+        // await this.saveData(this.settings);
+    }
 }
---
+++ src/metadata.ts
@@ -0,0 +1,63 @@
+import { DataAdapter, TFile, normalizePath } from "obsidian";
+import { path } from "src/path";
+
+
+/**
+ * 文件的元数据
+ */
+class Metadata {
+    /** 文件路径 */
+    path: string;
+
+    /** 文件名（带扩展名） */
+    name: string;
+
+    /** 文件的基名（不含扩展名） */
+    basename: string;
+
+    /** 文件扩展名 */
+    extension: string;
+
+    /** 文件的父路径 */
+    parentPath = "";
+
+    /** 文件的父路径基名 */
+    parentName = "";
+
+    attachmentFile?: TFile;
+
+    constructor(
+        path: string,
+        name: string,
+        basename: string,
+        extension: string,
+        parentPath: string,
+        parentName: string,
+        attachmentFile?: TFile,
+    ) {
+        this.path = path;
+        this.name = name;
+        this.basename = basename;
+        this.extension = extension;
+        this.parentPath = parentPath;
+        this.parentName = parentName;
+        this.attachmentFile = attachmentFile;
+    }
+
+}
+
+/**
+ * 为给定的文件路径返回一个新的 Metadata 实例。
+ *
+ * @param {string} file - The full path to the file.
+ * @return {Metadata} A new instance of Metadata containing information about the file.
+ */
+export function getMetadata(file: string, attach?: TFile): Metadata {
+    const parentPath = path.dirname(file);
+    const parentName = path.basename(parentPath);
+    const name = path.basename(file);
+    const extension = path.extname(file);
+    const basename = path.basename(file, extension);
+
+    return new Metadata(file, name, basename, extension, parentPath, parentName, attach);
+}
